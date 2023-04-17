9e204f2334fea2e0514c661f84fb60f70e1891ca
NaVi <tianyin@gcg.moe>
2023-04-17 16:33:42 +0800

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -97,17 +97,27 @@
 // const style2 ={
 //   color:'green',
 // }
+import { click } from "@testing-library/user-event/dist/click"
 import React from "react"
 
 function Hello () {
-  return <div>hello</div>
+  const clickHandler = (e, msg) => {
+    // e.preventDefault()
+    console.log('triggered click handler', e, msg)
+  }
+  return <div onClick={(e) => clickHandler(e, 'this is a msg')}>113</div>
 }
 
 class HelloComponent extends React.Component {
+  clickHandler = () => {
+    console.log('triggered click handler')
+  }
   render () {
-    return <div>this is a class Component</div>
+    return <div onClick={this.clickHandler}>this is a class Component</div>
   }
 }
+
+
 
 function App () {
   return (
@@ -124,3 +134,14 @@ function App () {
 }
 
 export default App
+
+// 面向对象语言中 this 表示当前对象的一个引用。
+
+// 但在 JavaScript 中 this 不是固定不变的，它会随着执行环境的改变而改变。
+
+// 在方法中，this 表示该方法所属的对象。
+// 如果单独使用，this 表示全局对象。
+// 在函数中，this 表示全局对象。
+// 在函数中，在严格模式下，this 是未定义的(undefined)。
+// 在事件中，this 表示接收事件的元素。
+// 类似 call() 和 apply() 方法可以将 this 引用到任何对象。
